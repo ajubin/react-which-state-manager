40cd096318e8e3b543b35b78ef99cabd2662490a
antoine jubin <antoine.jubin@depinfonancy.net>
2020-05-08 19:42:26 +0200

    feat(Hooks): create counter with hook

---
+++ src/hooks/Counter.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import { BaseCounter } from '../components/BaseCounter';
+
+export const Counter = () => {
+  const [value, setValue] = useState(0);
+  const increment = () => {
+    setValue(value + 1);
+  };
+
+  return <BaseCounter value={value} onPress={increment} />;
+};

0b562aa23b4ebab33ce6ab915b357d35a24bcba3
antoine jubin <antoine.jubin@depinfonancy.net>
2020-05-10 18:39:15 +0200

    mobx: add counter

---
+++ src/mobx/Counter.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BaseCounter } from '../components/BaseCounter';
+import { observer } from 'mobx-react';
+import { observable } from 'mobx';
+
+// ----------------- COUNTER MODEL ---------------------
+
+class CounterStore {
+  @observable counter = 0;
+
+  increment() {
+    this.counter++;
+  }
+
+  decrement() {
+    this.counter--;
+  }
+}
+
+const store = new CounterStore();
+
+export const Counter = observer(() => {
+  const increment = () => {
+    store.increment();
+  };
+  return <BaseCounter value={store.counter} onPress={increment} />;
+});
---
+++ src/mobx/index.ts
@@ -0,0 +1,1 @@
+export { Counter as MobXCounter } from './Counter';

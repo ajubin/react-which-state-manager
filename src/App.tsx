51ef41996819361c8988b4a0e86ae6890a3cc4ec
antoine jubin <antoine.jubin@depinfonancy.net>
2020-05-10 18:39:32 +0200

    refactor: create section

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -11,27 +11,39 @@ import { Provider as ReduxProvider } from 'react-redux';
 import { ReduxCounter, reduxStore } from './redux';
 
 import { XStateCounter } from './xstate';
+import { MobXCounter } from './mobx';
+
+const Section: FunctionComponent<{ title: string }> = ({ title, children }) => (
+  <div style={{ flexDirection: 'row', display: 'flex' }}>
+    <text>{title}</text>
+    <div style={{ flexDirection: 'column' }}>{children}</div>
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <HooksCounter />
-        <ApolloProvider client={apolloClient}>
-          <ApolloCounter />
-        </ApolloProvider>
-        <ReduxProvider store={reduxStore}>
-          <ReduxCounter />
-        </ReduxProvider>
-        <XStateCounter />
-
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
+        <Section title="Hooks">
+          <HooksCounter />
+        </Section>
+        <Section title="Apollo">
+          <ApolloProvider client={apolloClient}>
+            <ApolloCounter />
+          </ApolloProvider>
+        </Section>
+        <Section title="Redux">
+          <ReduxProvider store={reduxStore}>
+            <ReduxCounter />
+          </ReduxProvider>
+        </Section>
+        <Section title="XState">
+          <XStateCounter />
+        </Section>
+        <Section title="Mobx">
+          <MobXCounter />
+        </Section>
       </header>
     </div>
   );
